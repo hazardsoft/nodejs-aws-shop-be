f2705d8f4a720e39af7f979ccfe2ac9670e1eb57
Henadzi Shutko <hazardsoft@gmail.com>
2023-12-08 22:44:28 +0300

    fix: anticipate several records from queue, handle them in a batch

--- product-service/src/lambdas/createManyProducts.ts
+++ product-service/src/lambdas/createManyProducts.ts
@@ -6,16 +6,20 @@ export const handler = async (event: SQSEvent): Promise<void> => {
   console.log(`handling ${event.Records.length} messages from products queue`);
 
   try {
-    for await (const record of event.Records) {
-      const products = <AvailableProduct[]>JSON.parse(record.body);
+    const products: AvailableProduct[] = [];
 
+    for (const record of event.Records) {
       console.log(
-        `handling message: id ${record.messageId}, payload ${JSON.stringify(
-          products,
-        )}`,
+        `handling message: id ${record.messageId}, payload ${record.body}`,
       );
-      await createManyProducts(products);
+      const payload = <unknown>JSON.parse(record.body);
+      if (Array.isArray(payload)) {
+        products.push(...(<AvailableProduct[]>payload));
+      } else {
+        products.push(<AvailableProduct>payload);
+      }
     }
+    await createManyProducts(products);
   } catch (e) {
     console.error(JSON.stringify(e));
   }

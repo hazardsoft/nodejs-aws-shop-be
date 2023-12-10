9788c64bc31ad68f0dc0938c1712f76892ddae42
Henadzi Shutko <hazardsoft@gmail.com>
2023-12-11 00:42:05 +0300

    fix: split products to send to SQS into batches with no more then 10 messages

--- import-service/src/queue.ts
+++ import-service/src/queue.ts
@@ -4,17 +4,14 @@ import { SendMessageError } from "./errors";
 import { Product } from "./types";
 
 const sqsClient = new SQSClient();
+const MAX_MESSAGES_IN_BATCH = 10;
 
-export const sendMessageBatch = async (
+const sendMessageBatch_internal = async (
   queueUrl: string,
   products: Product[],
 ): Promise<void> => {
-  console.log(
-    `Sending batch message to queue ${queueUrl}, products: ${JSON.stringify(
-      products,
-    )}`,
-  );
-  const queueResult = await sqsClient.send(
+  console.log(`Sending batch message with x${products.length} products`);
+  const sendBatchResult = await sqsClient.send(
     new SendMessageBatchCommand({
       QueueUrl: queueUrl,
       Entries: products.map((product, index) => {
@@ -27,9 +24,34 @@ export const sendMessageBatch = async (
   );
 
   console.log(
-    `Message sent with status code ${queueResult.$metadata.httpStatusCode}`,
+    `Message sent with status code ${sendBatchResult.$metadata.httpStatusCode}`,
   );
-  if (queueResult.$metadata.httpStatusCode !== HTTP_STATUS_CODES.OK) {
+  if (sendBatchResult.$metadata.httpStatusCode !== HTTP_STATUS_CODES.OK) {
     throw new SendMessageError();
   }
+};
+
+export const sendMessageBatch = async (
+  queueUrl: string,
+  products: Product[],
+): Promise<void> => {
+  if (!products.length) {
+    console.log(`No products to send to queue ${queueUrl}`);
+    return;
+  }
+
+  const batchesCount = Math.ceil(products.length / MAX_MESSAGES_IN_BATCH);
+  console.log(
+    `Sending (${batchesCount}) batch(es) of total x${products.length} messages to queue ${queueUrl}`,
+  );
+  const batchesPromises = [];
+  for (let i = 0; i < batchesCount; i++) {
+    const batch = products.slice(
+      i * MAX_MESSAGES_IN_BATCH,
+      (i + 1) * MAX_MESSAGES_IN_BATCH,
+    );
+    batchesPromises.push(sendMessageBatch_internal(queueUrl, batch));
+  }
+  await Promise.allSettled(batchesPromises);
+  console.log(`All batches sent to queue ${queueUrl} successfully`);
 };

d807718fe5d04488f3daf6d8c4e1014b085b1d88
Henadzi Shutko <hazardsoft@gmail.com>
2023-12-17 09:07:20 +0300

    fix: make auth handler async; return deny policy in case an error occurs

--- authorization-service/src/lambdas/basicAuthorizer.ts
+++ authorization-service/src/lambdas/basicAuthorizer.ts
@@ -1,41 +1,58 @@
-import { APIGatewayTokenAuthorizerEvent } from "aws-lambda";
+import {
+  APIGatewayTokenAuthorizerEvent,
+  APIGatewayAuthorizerResult,
+} from "aws-lambda";
 
 const userName = process.env.USERNAME ?? "";
 const userPassword = process.env.USERPASS ?? "";
 
-type PolicyDocument = {
-  Version: "2012-10-17";
-  Statement: [
-    {
-      Sid: "BasicAuthorizerPolicy";
-      Effect: "Allow" | "Deny";
-      Action: ["execute-api:Invoke"];
-      Resource: string;
-    },
-  ];
-};
-
-export const handler = (
-  event: APIGatewayTokenAuthorizerEvent,
-): PolicyDocument => {
-  console.log(`basic authorizer, event: ${JSON.stringify(event)}`);
-
-  const authToken = event.authorizationToken.replace("Basic ", "");
-  const buf = Buffer.from(authToken, "base64");
-  const [uName, uPass] = buf.toString("utf-8").split(":");
-
-  const isCorrectCredentials = userName === uName && userPassword === uPass;
-  const policy: PolicyDocument = {
+const defaultAuthoriserResult: APIGatewayAuthorizerResult = {
+  principalId: "user",
+  policyDocument: {
     Version: "2012-10-17",
     Statement: [
       {
-        Sid: "BasicAuthorizerPolicy",
-        Effect: isCorrectCredentials ? "Allow" : "Deny",
-        Action: ["execute-api:Invoke"],
-        Resource: event.methodArn,
+        Action: "execute-api:Invoke",
+        Effect: "Deny",
+        Resource: "*",
       },
     ],
-  };
-  console.log(`generated policy: ${JSON.stringify(policy)}`);
-  return policy;
+  },
+};
+
+export const handler = async (
+  event: APIGatewayTokenAuthorizerEvent,
+): Promise<APIGatewayAuthorizerResult> => {
+  try {
+    console.log(`basic authorizer, event: ${JSON.stringify(event)}`);
+
+    const authToken = event.authorizationToken.replace("Basic ", "");
+    const buf = Buffer.from(authToken, "base64");
+    const [uName, uPass] = buf.toString("utf-8").split(":");
+
+    const isAuthenticated = userName === uName && userPassword === uPass;
+    const effect = isAuthenticated ? "Allow" : "Deny";
+
+    const authorizerResult: APIGatewayAuthorizerResult = {
+      principalId: userName,
+      policyDocument: {
+        Version: defaultAuthoriserResult.policyDocument.Version,
+        Statement: [
+          {
+            Action: "execute-api:Invoke",
+            Effect: effect,
+            Resource: event.methodArn,
+          },
+        ],
+      },
+    };
+
+    console.log(
+      `generated authorizer result: ${JSON.stringify(authorizerResult)}`,
+    );
+    return Promise.resolve(authorizerResult);
+  } catch (error) {
+    console.error(`error occurred in lambda authorizer!`);
+    return Promise.resolve(defaultAuthoriserResult);
+  }
 };

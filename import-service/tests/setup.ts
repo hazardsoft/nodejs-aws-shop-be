e4652da9e4531b096b4e53e317528d94ea2f651e
Henadzi Shutko <hazardsoft@gmail.com>
2024-07-05 22:37:10 +0300

    refactor: move env stub for unit tests to env.ts

--- import-service/tests/setup.ts
+++ import-service/tests/setup.ts
@@ -1,4 +1,3 @@
-import { vi } from 'vitest'
 import { createReadStream } from 'node:fs'
 import { fileURLToPath } from 'node:url'
 import type { ProductInput } from '@/types'
@@ -8,8 +7,6 @@ export const config = {
   bucketName: 'testBucket',
   fileName: 'test.csv'
 }
-
-vi.stubEnv('BUCKET_NAME', config.bucketName)
 
 export const getProductsStream = () => {
   return createReadStream(fileURLToPath(new URL('data/products.csv', import.meta.url)))

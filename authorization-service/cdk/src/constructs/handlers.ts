88fc9e72da153dca6476254dff5af2067d2fe399
Henadzi Shutko <hazardsoft@gmail.com>
2024-07-10 17:29:27 +0300

    feat: pass username/password as env vars to handler

--- authorization-service/cdk/src/constructs/handlers.ts
+++ authorization-service/cdk/src/constructs/handlers.ts
@@ -1,15 +1,24 @@
 import { Code, Function as LambdaFunction, Runtime, type IFunction } from 'aws-cdk-lib/aws-lambda'
 import { Construct } from 'constructs'
 
+interface AuthorizationHandlersProps {
+  username: string
+  password: string
+}
 export class AuthorizationHandlers extends Construct {
   public readonly authorizer: IFunction
-  constructor(scope: Construct, id: string) {
+
+  constructor(scope: Construct, id: string, props: AuthorizationHandlersProps) {
     super(scope, id)
 
     this.authorizer = new LambdaFunction(this, 'BasicAuthorizer', {
       code: Code.fromAsset('../dist/handlers/basicAuthorizer'),
       handler: 'basicAuthorizer.handler',
-      runtime: Runtime.NODEJS_20_X
+      runtime: Runtime.NODEJS_20_X,
+      environment: {
+        USERNAME: props.username,
+        PASSWORD: props.password
+      }
     })
   }
 }
--- authorization-service/cdk/src/stack.ts
+++ authorization-service/cdk/src/stack.ts
@@ -1,11 +1,17 @@
 import { Stack } from 'aws-cdk-lib'
 import { AuthorizationHandlers } from './constructs/handlers'
 import type { Construct } from 'constructs'
+import { config } from './config.js'
 
 export class AuthorizationService extends Stack {
   constructor(scope: Construct, id: string) {
     super(scope, id)
 
-    new AuthorizationHandlers(this, 'AuthorizationHandlers')
+    const { username, password } = config
+
+    new AuthorizationHandlers(this, 'AuthorizationHandlers', {
+      username,
+      password
+    })
   }
 }

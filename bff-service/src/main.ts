3735f9aa293883749013433ee49eadbfc366f866
Henadzi Shutko <hazardsoft@gmail.com>
2024-08-11 22:49:41 +0300

    feat: configure http server with provided middlewares

--- bff-service/src/main.ts
+++ bff-service/src/main.ts
@@ -1,8 +1,25 @@
 import { BffService } from './server/server'
+import deleteMiddleware from './server/middleware/delete'
+import optionsMiddleware from './server/middleware/options'
+import jsonMiddleware from './server/middleware/json'
+import cacheMiddleware from './server/middleware/cache'
 import redirectMiddleware from './server/middleware/redirect'
+import type { PathMatcher, ServerRequest } from './server/types'
+import { match } from 'path-to-regexp'
 
 const port = Number(process.env.PORT) || 8080
 
+const productsMatcher: PathMatcher = (req: ServerRequest) => {
+  const matchProducts = match('/product', { decode: decodeURIComponent })
+  const originalUrl = new URL(req.url ?? '', `http://${req.headers.host}`)
+  const isProducts = req.method === 'GET' && matchProducts(originalUrl.pathname)
+  return !!isProducts
+}
+
 const bffService = new BffService()
+bffService.addMiddleware(deleteMiddleware) // do not allow DELETE method
+bffService.addMiddleware(optionsMiddleware) // return CORS headers on OPTIONS method
+bffService.addMiddleware(jsonMiddleware) // assign parsed JSON body to server request if applicable
+bffService.addMiddleware(cacheMiddleware, productsMatcher) // cache all products
 bffService.addMiddleware(redirectMiddleware) // redirects requests to responsible services
 bffService.listen(port)
